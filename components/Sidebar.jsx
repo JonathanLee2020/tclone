08def40ce65581ab284f632fc1dd45225c2dbfdc
Jonathan Lee <jonathanlee882@gmail.com>
2023-09-06 16:54:40 +1000

    added assets folder and sidebar links

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,15 +1,70 @@
-export default function Sidebar () {
+import {
+    HomeIcon,
+    HashtagIcon,
+    InboxIcon,
+    BookmarkIcon,
+    ClipboardListIcon,
+    BellIcon,
+    UserIcon,
+    DotsCircleHorizontalIcon
+} from "@heroicons/react/outline"
+
+function SideBarLink({text, Icon}) {
     return (
-        <>
-            <div>Logo</div>
-            <nav>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-                <li>Home</li>
-            </nav>
-            <div>User</div>
-        </>
+        <li className="flex items-center text-xl">
+            <Icon className = "h-7"/>
+            
+            <span className="xl:inline space-x-3">{text} </span>
+        </li>
     )
 }
+
+
+export default function Sidebar () {
+    return (
+        <div className="hidden sm:flex flex-col fixed">
+            <div>
+                <Image src={} width={34} height={34}></Image>
+            </div>
+            <nav>
+                <SideBarLink 
+                text={"Home"}
+                Icon={HomeIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"Explore"}
+                Icon={HashtagIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"Notifications"}
+                Icon={BellIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"Messages"}
+                Icon={InboxIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"Bookmarks"}
+                Icon={BookmarkIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"Profile"}
+                Icon={UserIcon}
+                ></SideBarLink>
+
+                <SideBarLink 
+                text={"More"}
+                Icon={DotsCircleHorizontalIcon}
+                ></SideBarLink>
+               
+            </nav>
+            <div>User</div>
+        </div>
+    )
+}
+
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,14 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <div className='bg-black min-h-screen'>
+    <div className='bg-black min-h-screen 
+    text-[#E7E9EA]
+    max-w-[1400px]
+    mx-auto
+    '
+    
+
+    >
     <Sidebar></Sidebar>
     </div>
   )
