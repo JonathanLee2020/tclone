23806fe181c00a61a8cf6fa0e4c39f541ece41c7
Jonathan Lee <jonathanlee882@gmail.com>
2023-09-08 19:21:38 +1000

    added button

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -12,24 +12,17 @@ import {
 import Image from 'next/image';
 
 
-function SideBarLink({text, Icon}) {
-    return (
-        <li className="flex items-center text-xl py-3">
-            <Icon className = "h-7"/>
-            
-            <span className=" hidden xl:inline space-x-3">{text} </span>
-        </li>
-    )
-}
+
 
 
 export default function Sidebar () {
     return (
-        <div className="hidden sm:flex flex-col fixed">
-            <div className="py-3">
-                <Image src={"/assets/twitter-logo.png"} width={34} height={34} alt="hello"/>
-            </div>
-            <nav>
+        <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+            
+            <nav className="h-full relative xl:space-y-1.5 xl">
+                <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
+                    <Image src={"/assets/twitter-logo.png"} width={34} height={34} alt="hello"/>
+                </div>
                 <SideBarLink 
                 text={"Home"}
                 Icon={HomeIcon}
@@ -64,10 +57,92 @@ export default function Sidebar () {
                 text={"More"}
                 Icon={DotsCircleHorizontalIcon}
                 ></SideBarLink>
+
+                <button className=" xl:inline bg-[#1d9bf0] rounded-full h-[52px] w-[200px]">
+                    Tweet
+                </button>
+            
+                <div className="absolute bottom-0">User</div>
                
             </nav>
-            <div>User</div>
         </div>
     )
 }
 
+
+function SideBarLink({text, Icon}) {
+    return (
+        <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
+            <Icon className = "h-7"/>
+            
+            <span className="hidden xl:inline space-x-3">{text} </span>
+        </li>
+    )
+}
+
+
+// import {
+//   HomeIcon,
+//   HashtagIcon,
+//   InboxIcon,
+//   BookmarkIcon,
+//   ClipboardListIcon,
+//   BellIcon,
+//   UserIcon,
+//   DotsCircleHorizontalIcon,
+//   DotsHorizontalIcon,
+// } from "@heroicons/react/outline";
+// import Image from "next/image";
+
+// export default function Sidebar() {
+
+
+
+//   return (
+//     <div className="h-full hidden sm:flex flex-col fixed xl:ml-24">
+//       <nav className="h-full relative xl:space-y-1.5">
+//         <div className="flex justify-center xl:justify-start items-center py-3 xl:p-3">
+//           <Image src={"/assets/twitter-logo.png"} width={34} height={34} />
+//         </div>
+//         <SidebarLink Icon={HomeIcon} text={"Home"} />
+//         <SidebarLink Icon={HashtagIcon} text={"Explore"} />
+//         <SidebarLink Icon={BellIcon} text={"Notifications"} />
+//         <SidebarLink Icon={InboxIcon} text={"Messages"} />
+//         <SidebarLink Icon={BookmarkIcon} text={"Bookmarks"} />
+//         <SidebarLink Icon={UserIcon} text={"Profile"} />
+//         <SidebarLink Icon={DotsCircleHorizontalIcon} text={"More"} />
+//         <button
+//           className="hidden xl:inline bg-[#1d9bf0]
+//         rounded-full h-[52px] mt-2 w-[200px] text-lg font-bold
+//         "
+//         >
+//           Tweet
+//         </button>
+//         <div
+//           className="
+//         bottom-0
+//         hover:bg-white hover:bg-opacity-10 rounded-full cursor-pointer
+//         absolute xl:p-3 flex justify-center items-center  space-x-3"
+//         >
+//           <img
+//             className="w-10 h-10 rounded-full
+//           object-cover
+//           "
+//           />
+//           <div className="hidden xl:inline">
+//           </div>
+//           <DotsHorizontalIcon className="h-5 hidden xl:inline" />
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// function SidebarLink({ text, Icon }) {
+//   return (
+//     <li className="hoverAnimation flex mb-3 xl:justify-start justify-center items-center text-xl space-x-3">
+//       <Icon className="h-7" />
+//       <span className="hidden xl:inline">{text}</span>
+//     </li>
+//   );
+// }
